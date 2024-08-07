a915b4d9ffc70dee54c59a1faad1db0548ec80e6
AshishKumar2851 <103657403+AshishKumar2851@users.noreply.github.com>
2024-08-07 12:32:06 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
 git testing for git_test remote repository
 editing from remote repo side. Which is not recommended at all.
-<<<<<<< HEAD
 Hello from local
-=======
 echo "Hello"
->>>>>>> dbf10869bda3f7ec9cb19558d83ce4425cb56045
